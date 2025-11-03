197f4d6797a6b863f3a1fb1f97eb7247eeefa85d
sandhuxvishav <vishavpartapsandhu43@gmail.com>
2025-11-03 06:28:47 +0000

    added hero section

---
+++ frontend-nerds/src/components/Sevices.jsx
@@ -0,0 +1,18 @@
+const Services = () => {
+    return (
+        <div className="flex flex-col  items-center min-h-dvh">
+            <h1 className="poppins-semibo">Our Services</h1>
+        </div>
+    )
+ }
+ const Service = () => {
+    return (
+        <div className="flex">
+        <div className="img">
+        <img src="" alt="" />
+        </div>
+        </div>
+    )
+ }
+
+ export default Services;
